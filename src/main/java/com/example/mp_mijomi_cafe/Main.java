d94b00ade6b252b528afa989bbdd4e4bfbea0c5d
eiaiae <deercia@up.edu.ph>
2023-01-13 19:21:38 +0800

    edited table css, pics, button

--- src/main/java/com/example/mp_mijomi_cafe/Main.java
+++ src/main/java/com/example/mp_mijomi_cafe/Main.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import java.io.IOException;
@@ -21,6 +22,16 @@ public class Main extends Application {
         return ingredientData;
     }
 
+    /**
+     *
+     * @param stage the primary stage for this application, onto which
+     * the application scene can be set.
+     * Applications may create other stages, if needed, but they will not be
+     * primary stages.
+     * @throws IOException
+     *
+     * Loads the start-view.fxml file
+     */
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("start-view.fxml"));
@@ -28,14 +39,26 @@ public class Main extends Application {
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+        stage.setTitle("Mijomi Inventory");
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
+
 
         IngredientController ingredientControl = loader.getController();
         ingredientControl.setMain(this);
     }
 
+    /**
+     *
+     * @param ingredient
+     * @return
+     * @throws IOException
+     *
+     * Loads the addNew.fxml file as a popUpWindow
+     */
     public boolean showPopUpWindow(Ingredient ingredient) throws IOException{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("editIngredient.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("addNew.fxml"));
         Parent root = loader.load();
 
         Stage popUpWindow = new Stage();
@@ -47,6 +70,8 @@ public class Main extends Application {
 
         popUpWindow.setScene(scene);
         popUpWindow.showAndWait();
+        popUpWindow.setResizable(false);
+        popUpWindow.getIcons().add(new Image(Main.class.getResourceAsStream("icon.png")));
 
         return editIngredientControl.okButtonIsClicked;
     }
