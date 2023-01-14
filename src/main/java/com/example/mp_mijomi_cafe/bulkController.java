f730f092ea51b4b3445554d54033c1a861f75b1f
aaeikllm <mdlayug@up.edu.ph>
2023-01-14 17:58:49 +0800

    bulk import working

--- src/main/java/com/example/mp_mijomi_cafe/bulkController.java
+++ src/main/java/com/example/mp_mijomi_cafe/bulkController.java
@@ -1,11 +1,8 @@
 package com.example.mp_mijomi_cafe;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
@@ -13,41 +10,77 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import static java.lang.Integer.parseInt;
-
 public class bulkController {
     @FXML
     private TextField fileName;
     private Stage popUpWindow;
-    private Ingredient ingredient;
     public boolean okButtonIsClicked = false;
+    private String file;
+    private String item;
+    private String category;
+    private String brand;
+    private int size;
+    private String unit;
+    private String color;
+    private String type;
+    private String description;
 
     public void setPopUpWindow(Stage popUpWindow){
         this.popUpWindow = popUpWindow;
     }
 
-    public void setIngredient(Ingredient ingredient){
-        this.ingredient = ingredient;
-    }
-
     public void okButtonClicked() throws FileNotFoundException {
-        String file = "/Users/joanatria/Desktop/mp_mijomi_cafe/src/main/java/com/example/mp_mijomi_cafe/bulk.csv";
+        file = fileName.getText();
         BufferedReader br = new BufferedReader(new FileReader(file));
         String s;
+
+        Ingredient ingredient = new Ingredient();
+
         try{
             while ((s = br.readLine()) != null) {
                 String[] split = s.split(",");
-                ingredient.setSKU(split[0]);
-                ingredient.setItem(split[1]);
-                ingredient.setCategory(split[2]);
-                ingredient.setBrand(split[3]);
-                ingredient.setItemSize(Integer.parseInt(split[4]));
-                ingredient.setUnit(split[5]);
-                ingredient.setColor(split[6]);
-                ingredient.setType(split[7]);
-                ingredient.setDescription(split[8]);
+                item = split[0];
+                category = split[1];
+                brand = split[2];
+                size = Integer.parseInt(split[3]);
+                unit = split[4];
+
+                if (split[5].equals("-")) {
+                    color = "";
+                } else {
+                    color = split[5];
+                }
+
+                if (split[6].equals("-")) {
+                    type = "";
+                } else {
+                    type = split[6];
+                }
+
+                if (split[7].equals("-")) {
+                    description = "";
+                } else {
+                    description = split[7];
+                }
+
+                ingredient.setItem(item);
+                ingredient.setCategory(category);
+                ingredient.setBrand(brand);
+                ingredient.setItemSize(size);
+                ingredient.setUnit(unit);
+                ingredient.setColor(color);
+                ingredient.setType(type);
+                ingredient.setDescription(description);
+                ingredient.setSKU(IngredientController.generateSKU(ingredient));
+
+                IngredientController.addToSQL(ingredient);
             }
         } catch(IOException e){e.printStackTrace();}
+        catch (NumberFormatException e){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setContentText("Amount should be an integer.");
+            alert.show();
+        };
 
         okButtonIsClicked = true;
         popUpWindow.close();
